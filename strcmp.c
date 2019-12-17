520c719efd8ae8cdccd9c8981808332738d08d30
Mathieu Rabeyrin <m.rabeyrin0@gmail.com>
2019-12-17 20:06:06 +0100

    Create strcmp.c

---
+++ strcmp.c
@@ -0,0 +1,10 @@
+int cmp(const char *s1, const char *s2)
+{
+    for	(; *s1 != '\0' && *s2 != '\0';) {
+        if (*s1 != *s2)
+            return (*s1 - *s2);
+	      *s1++;
+        *s2++;
+    }
+    return (0);
+}
