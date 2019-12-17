cb979e68f9f59e31475c159a145cd4715fad0fba
Mathieu Rabeyrin <m.rabeyrin0@gmail.com>
2019-12-17 20:19:04 +0100

    Update strcmp.c

--- strcmp.c
+++ strcmp.c
@@ -1,6 +1,6 @@
 int cmp(const char *s1, const char *s2)
 {
-    for	(; *s1 != '\0' && *s2 != '\0';) {
+    for	(; *s1 != '\0' || *s2 != '\0';) {
         if (*s1 != *s2)
             return (*s1 - *s2);
 	*s1++;
