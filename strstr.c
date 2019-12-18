54dacad2cec9947234c69d2b7de85d961b6cb031
Mathieu Rabeyrin <m.rabeyrin0@gmail.com>
2019-12-18 15:42:52 +0100

    Update strstr.c

--- strstr.c
+++ strstr.c
@@ -3,9 +3,8 @@ char *str(const char *haystack, const char *needle)
     for (; *haystack != '\0'; *haystack++) {
         if (*haystack == *needle) {
             for (int index = 0; *needle != '\0'; index++) {
-                if (*needle != haystack[index]) {
+                if (*needle != haystack[index])
                     return (NULL);
-                }
                 *needle++;
             }
             return (char *) haystack;
