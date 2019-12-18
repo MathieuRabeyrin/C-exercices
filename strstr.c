287a37ff9ee6a803702e32e8e10bbb87b863914f
Mathieu Rabeyrin <m.rabeyrin0@gmail.com>
2019-12-18 15:40:26 +0100

    Create strstr.c

---
+++ strstr.c
@@ -0,0 +1,15 @@
+char *str(const char *haystack, const char *needle)
+{
+    for (; *haystack != '\0'; *haystack++) {
+        if (*haystack == *needle) {
+            for (int index = 0; *needle != '\0'; index++) {
+                if (*needle != haystack[index]) {
+                    return (NULL);
+                }
+                *needle++;
+            }
+            return (char *) haystack;
+        }
+    }
+    return (NULL);
+}
