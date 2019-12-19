08d9ffbe9989d056db86db6179ca9575d55fad42
Mathieu Rabeyrin <m.rabeyrin0@gmail.com>
2019-12-19 12:33:41 +0100

    Delete strcpy.c

--- strcpy.c
+++
@@ -1,9 +0,0 @@
-char *cpy(char *dest, const char *src)
-{
-    int index = 0;
-    
-    for (; src[index] != '\0'; index++)
-        dest[index] = src[index];
-    dest[index] = '\0';
-    return (dest);
-}
